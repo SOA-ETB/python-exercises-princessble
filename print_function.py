30c518388417ff476cd8f5c14bd2d3f820c2afed
Blessing Fortune Kwomo <74838811+princessble@users.noreply.github.com>
2025-07-31 12:40:15 +0100

    Add files via upload
    # Exercise 1
    #Use the "print" function to print the line "Hello, World!".
    
    print("Hello, World!")
    
    Hello, World!
    
    #The above exercise demonstrates the print() function in python.
    #The print function print() is used to display text or output to the console or terminal
    #The  text must enclosed in either  single quotes '' or double quotes "".
    #In the above exercise, the script prints: Hello, World!

---
+++ print_function.py
@@ -0,0 +1,9 @@
+# Exercise 1
+#Use the "print" function to print the line "Hello, World!".
+
+print("Hello, World!")
+
+#The above exercise demonstrates the print() function in python.
+#The print function print() is used to display text or output to the console or terminal
+#The  text must enclosed in either  single quotes '' or double quotes "".
+#In the above exercise, the script prints: Hello, World!
