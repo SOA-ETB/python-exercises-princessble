c98994d0bcaca7f4a9b5d1887fa7ce0cfb31f887
Blessing Fortune Kwomo <74838811+princessble@users.noreply.github.com>
2025-07-31 13:21:19 +0100

    Add Exercise 3: Lists and strings with printing examples
    ## Exercise 3: Lists and Strings
    
    - numbers is a list of three integers, e.g., [3, 7, 21].
    - strings is a single string that contains the words "hello" and "world", e.g., "hello world".
    - names is a list with at least three strings, e.g., ["Sam", "Blessing", "Ayman"].
    
    My Program prints:
    1. the full numbers list,
    2. the strings value,
    3. the *second* name from names (index 1)

---
+++ exercise_3_lists_and_strings.py
@@ -0,0 +1,23 @@
+#Exercise 3: Lists and Strings
+
+# 1. Create the variables
+# numbers: a list of 3 integers
+numbers = [3, 7, 21]
+
+#strings: a single string that contains "hello" and "world"
+strings = "hello world"  #you could also use "hello, world"
+
+#names: a list with at least 3 strings
+names = ["Sam", "Blessing", "Ayman"]
+
+#2. Print the full list of numbers
+print("numbers:", numbers)
+
+
+#3 print strings
+print("strings:", strings)
+
+#4. Print the second name in the names list
+#lists are zero indexed, so index 1  is the second item.
+
+print("second name:", names[1])
