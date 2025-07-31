2d1c9b66013168b4152201ee9ae532a11fc96a14
Blessing Fortune Kwomo <74838811+princessble@users.noreply.github.com>
2025-07-31 13:01:42 +0100

    Add python variable exercise no 2 (string, int, float)
    ## Exercise 2: Variables (String, Float, Integer)
    
    This exercise  is to create
    - mystring → a string containing "hello"
    - myfloat → a floating-point number 10.0
    - myint → an integer 20
    
    ```python
    mystring = "hello"
    myfloat = 10.0
    myint = 20

---
+++ exercise2_variables.py
@@ -0,0 +1,16 @@
+#Exercise no 2: Create a string, a float, and an integer
+
+#String variable named 'mystring' that contains the word "hello"
+mystring  = "hello"
+
+#Floating-point variable named 'myfloat' that contains the number 10.0
+myfloat = 10.0
+
+#Integer variable named 'myint' that contains the number 20
+myint = 20
+
+
+#(optional) print them so you can see  the values
+print("mystring:", mystring)
+print("myfloat:",  myfloat)
+print("myint:", myint)
